218fff37cc335e480d02b98f07fe99f66b2a1ade
K <test>
2023-01-26 21:17:30 +0900

    temp commit

---
+++ app/src/main/java/jp/gr/java_conf/sake_beer/bttest/ProfileFragment.kt
@@ -0,0 +1,75 @@
+package jp.gr.java_conf.sake_beer.bttest
+
+import android.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.fragment.app.Fragment
+import jp.gr.java_conf.sake_beer.bttest.databinding.FragmentProfileBinding
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ProfileFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ProfileFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    private lateinit var binding: FragmentProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        binding.infoBTText="Not connected"
+        binding.ageText="45"
+        binding.heightText="160"
+        binding.weightText="80"
+        binding.commentText="comment..."
+        binding.infoText="info..."
+
+        return binding.root
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment Profileragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            ProfileFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
---
+++ app/src/main/java/jp/gr/java_conf/sake_beer/bttest/ViewModel.kt
@@ -0,0 +1,10 @@
+package jp.gr.java_conf.sake_beer.bttest
+
+import androidx.databinding.BaseObservable
+
+
+
+class ViewModel : BaseObservable() {
+
+
+}
